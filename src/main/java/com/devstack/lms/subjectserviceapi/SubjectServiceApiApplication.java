336436ed71069c5f262ce20081143cdc1801b72a
ozen-hub <info.ozenhub@gmail.com>
2024-03-13 20:28:17 +0530

    config eureka

--- src/main/java/com/devstack/lms/subjectserviceapi/SubjectServiceApiApplication.java
+++ src/main/java/com/devstack/lms/subjectserviceapi/SubjectServiceApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SubjectServiceApiApplication {
 
 	public static void main(String[] args) {
